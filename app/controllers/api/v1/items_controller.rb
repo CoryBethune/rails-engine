f39f3c9fb7b0913296ba2d1d10fc46ec95d4ce37
Cory Bethune <corybbethune@gmail.com>
2022-08-31 23:25:12 -0700

    Add one item method to item serializer. Passes rspec and postman tests

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::ItemsController < ApplicationController
 
   def show
     item = Item.find(params[:id])
-    render json: ItemSerializer.format_items(item)
+    render json: ItemSerializer.one_item(item)
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -43,6 +43,22 @@ RSpec.describe 'Items API' do
 
     expect(response).to be_successful
 
+    expect(item.count).to eq(3)
+    expect(item[:attributes].count).to eq(4)
 
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to be_an(String)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_an(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_an(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_an(Float)
   end
 end
