01a6386824cf01c3676153aa196c6c17c30c19ff
Cory Bethune <corybbethune@gmail.com>
2022-08-31 17:24:10 -0700

    Create serializers for mercant model. create tests. tests pass.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,23 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
-  it "test" do
+  it "gets all the merchants" do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchants = response_body[:data]
 
     expect(merchants.count).to eq(3)
 
     merchants.each do |merchant|
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
-
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_an(String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_an(String)
     end
   end
+
+  it "shows the details of one merchant" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchant = response_body[:data]
+    
+    expect(response).to be_successful
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_an(String)
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to be_an(String)
+  end
+
+
 end
