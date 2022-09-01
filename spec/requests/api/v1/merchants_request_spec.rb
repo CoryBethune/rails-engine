57854209bfcd59cb7d9963d16680401c1f3ddc9c
Cory Bethune <corybbethune@gmail.com>
2022-08-31 22:17:24 -0700

    Get a merchant items User story passes tests.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -32,18 +32,49 @@ RSpec.describe 'Merchants API' do
 
     response_body = JSON.parse(response.body, symbolize_names: true)
     merchant = response_body[:data]
-    
-    expect(response).to be_successful
 
-    expect(merchant[:attributes]).to have_key(:name)
-    expect(merchant[:attributes][:name]).to be_an(String)
+    expect(response).to be_successful
 
     expect(merchant).to have_key(:id)
     expect(merchant[:id]).to be_an(String)
 
     expect(merchant).to have_key(:type)
     expect(merchant[:type]).to be_an(String)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_an(String)
   end
 
+  it "gets a merchants items" do
+    id = create(:merchant).id
+    item = create(:item, merchant_id: id)
 
+    get "/api/v1/merchants/#{id}/items"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    items = response_body[:data]
+
+    expect(response).to be_successful
+
+    expect(items[0].count).to eq(3)
+    expect(items[0][:attributes].count).to eq(4)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_an(Float)
+    end
+
+  end
 end
